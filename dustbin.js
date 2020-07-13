9ef78bdba4d31cdd695b407e033af58a29f5da90
KritiPatil <66488628+KritiPatil@users.noreply.github.com>
2020-07-13 13:25:38 -0900

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -1,23 +1,5 @@
-class Dustbin{
+class Dustbin extends BaseClass{
    constructor(x, y, width, height) {
-       var options = {
-           'restitution':0.8,
-           'friction':1.0,
-           'density':1000
+       super(x, y, width, height);
        }
-       this.body = Bodies.rectangle(x, y, width, height, options);
-       this.width = width;
-       this.height = height;
-       this.image = loadImage("dustbingreen.png");
-       World.add(world, this.body);
-     }
-     display(){
-       var angle = this.body.angle;
-       push();
-       translate(this.body.position.x, this.body.position.y);
-       rotate(angle);
-       imageMode(CENTER);
-       image(this.image, 0, 0, this.width, this.height);
-       pop();
-     }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground;
-var db1, db2, db3, db4, paper;
+var db1, db2, db3, paper;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -20,20 +20,20 @@ function setup() {
 	world = engine.world;
 
 
-	packageBody = new Paper(100, 300, 40);
+	packageBody = new Paper(100, 300, 50, 50);
 	/*packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
 	World.add(world, packageBody);*/
 	
 
 	//Create a Ground
-	 
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
 
-	 db1 = new Dustbin(670, 640, 150, 10);
-	 db2 = new Dustbin(620, 640, 10, 80);
-	 db3 = new Dustbin(720, 640, 10, 80);
-	 db4 = createSprite(770, 640, 10, 1400);
+	 db1 = new Dustbin(670, 640, 200, 180);
+	 //db2 = new Dustbin(570, 640, 10, 60);
+	 //db3 = new Dustbin(750, 640, 10, 70);
 
-	 packageBody.collide("db4");
+	 //paper = new Paper(100, 640, 50);
 
 	Engine.run(engine);
   
@@ -45,24 +45,19 @@ function draw() {
   background(0);
 
   db1.display();
-  db2.display();
-  db3.display();
-  db4.display();
-
-  ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	ground.shapeColor = "yellow";
-	 World.add(world, ground);
-
   packageBody.display();
 
-  KeyPressed();
-  
+  keyPressed();
+
+  fill ("yellow");
+  rect(ground.position.x, ground.position.y, width, 10);
+
   drawSprites();
  
 }
 
-function KeyPressed() {
-	if(keyCode === UP_ARROW) {
-	  Matter.Body.applyForce(packageBody.body, packageBody.body.position, {x:10, y:-18});
+function keyPressed() {
+	if(keyCode 	=== UP_ARROW) {
+       Matter.Body.applyForce(packageBody.body, packageBody.body.position, {x:1, y:-11.6});
 	}
 }
